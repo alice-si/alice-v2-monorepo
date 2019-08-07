b1ce5ba083c0a9712f1f978434950356527862c3
jakub-wojciechowski <jakub@alice.si>
2019-08-07 14:25:54 +0100

    Escrow unlocking

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.2;
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
@@ -9,22 +8,40 @@ import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
  * @dev Contract dedicated to hole funds until impact is validated.
  *
  */
-contract Escrow is Ownable {
+contract Escrow {
     using SafeMath for uint256;
 
-    event Validated(uint256 amount);
+    event Unlocked(uint256 amount);
     event Withdrawn(address indexed receiver, uint256 amount);
 
+    /**
+     * @dev Throws if called by any account other than the operator.
+     */
+    modifier onlyOperator() {
+        require(msg.sender == operator, "The caller is not the operator");
+        _;
+    }
+
     ERC20 public paymentToken;
+    address public operator;
+
+
+    uint256 public unlocked;
+    uint256 public withdrawn;
     uint256 public capacity;
-    uint256 public validated;
-    address public validator;
 
-
-    constructor(ERC20 _paymentToken, uint256 _capacity, address _validator) public {
+    constructor(ERC20 _paymentToken, uint256 _capacity, address _operator) public {
         paymentToken = _paymentToken;
         capacity = _capacity;
-        validator = _validator;
+        operator = _operator;
+    }
+
+
+    function unlock(uint256 _amount) public onlyOperator {
+        unlocked = unlocked.add(_amount);
+        require(unlocked <= capacity, "Cannot unlock more than the max capacity of the escrow");
+
+        emit Unlocked(_amount);
     }
 
 
@@ -33,7 +50,8 @@ contract Escrow is Ownable {
      * @param _amount The amount of funds intended to be taken out
      */
     function withdraw(uint256 _amount) public {
-        //TODO: Validate amount
+        withdrawn = withdrawn.add(_amount);
+        require(withdrawn <= unlocked, "Cannot withdraw more funds than has been unlocked");
 
         require(paymentToken.transfer(msg.sender, _amount));
 
--- test/escrow.test.js
+++ test/escrow.test.js
@@ -3,13 +3,13 @@ var GBP = artifacts.require("DigitalGBPToken");
 
 require("./test-setup");
 
-contract('Escrow', function ([owner, validator, receiver]) {
+contract('Escrow', function ([owner, operator, receiver]) {
   var escrow;
   var gbp;
 
   before("deploy escrow and token contracts", async function () {
     gbp = await GBP.new();
-    escrow = await Escrow.new(gbp.address, 1000, validator);
+    escrow = await Escrow.new(gbp.address, 1000, operator);
   });
 
   it("should deposit", async function () {
@@ -17,8 +17,15 @@ contract('Escrow', function ([owner, validator, receiver]) {
     (await gbp.balanceOf(escrow.address)).should.be.bignumber.equal('1000');
   });
 
+  it("should not withdraw until unlocked", async function () {
+    await escrow.withdraw(100, {from: receiver}).shouldBeReverted();
+  });
+
   it("should withdraw", async function () {
+    await escrow.unlock(100, {from: operator});
+
     await escrow.withdraw(100, {from: receiver});
+
     (await gbp.balanceOf(escrow.address)).should.be.bignumber.equal('900');
     (await gbp.balanceOf(receiver)).should.be.bignumber.equal('100');
   });
