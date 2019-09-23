0fc28cf324e8ba35c779f7c623df13d8de4adc1b
jakub-wojciechowski <jakub@alice.si>
2019-09-23 10:55:14 +0100

    local/public chain connection

--- src/main.js
+++ src/main.js
@@ -13,14 +13,9 @@ Vue.use(VueMaterial)
 
 
 window.addEventListener('load', function () {
-  if (typeof web3 !== 'undefined') {
-    console.log('Web3 injected browser: OK.')
-    window.web3 = new Web3(window.web3.currentProvider)
-  } else {
-    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-  }
+  let host = window.location.hostname;
+  let providerUrl =  (host === 'localhost') ? 'localhost:8545' : 'http://ganache.demo.alice.si:80';
+  window.web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
   /* eslint-disable no-new */
   new Vue({
