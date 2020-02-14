f6b424310926dd40f9e57174a43079e1c22f416f
jakub-wojciechowski <jakub@alice.si>
2020-02-14 12:37:20 +0000

    Update escrow tests

--- test/escrow.test.js
+++ test/escrow.test.js
@@ -3,13 +3,14 @@ var GBP = artifacts.require("AliceUSD");
 
 require("./test-setup");
 
-contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
+contract('Escrow', function ([owner, recipient, unauthorised]) {
   var escrow;
   var gbp;
 
   before("deploy escrow and token contracts", async function () {
     gbp = await GBP.new();
-    escrow = await Escrow.new(gbp.address, 1000, operator, receiver);
+    escrow = await Escrow.new(gbp.address, 1000);
+    await escrow.setRecipient(recipient);
   });
 
   it("should deposit", async function () {
@@ -18,7 +19,7 @@ contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
   });
 
   it("should not withdraw until unlocked", async function () {
-    await escrow.withdraw(receiver, 100, {from: receiver}).shouldBeReverted("VM Exception while processing transaction: revert Cannot withdraw more funds than has been unlocked");
+    await escrow.withdraw(recipient, 100, {from: recipient}).shouldBeReverted("VM Exception while processing transaction: revert Cannot withdraw more funds than has been unlocked");
   });
 
   it("should not unlock from unauthorised until unlocked", async function () {
@@ -26,24 +27,21 @@ contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
   });
 
   it("should unlock", async function () {
-    await escrow.unlock(100, {from: operator});
+    await escrow.unlock(100, {from: owner});
     (await escrow.unlocked()).should.be.bignumber.equal('100');
   });
 
   it("should not withdraw from unauthorised", async function () {
-    await escrow.withdraw(receiver, 100, {from: unauthorised}).shouldBeReverted();
+    await escrow.withdraw(recipient, 100, {from: unauthorised}).shouldBeReverted();
   });
 
   it("should withdraw", async function () {
-    await escrow.withdraw(receiver, 100, {from: receiver});
+    await escrow.withdraw(recipient, 100, {from: recipient});
 
     (await escrow.withdrawn()).should.be.bignumber.equal('100');
 
     (await gbp.balanceOf(escrow.address)).should.be.bignumber.equal('900');
-    (await gbp.balanceOf(receiver)).should.be.bignumber.equal('100');
+    (await gbp.balanceOf(recipient)).should.be.bignumber.equal('100');
   });
 
 });
-
-
-
--- test/refundable-escrow.test.js
+++ test/refundable-escrow.test.js
@@ -3,13 +3,14 @@ var GBP = artifacts.require("AliceUSD");
 
 require("./test-setup");
 
-contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
+contract('Escrow', function ([owner, recipient, unauthorised]) {
   var escrow;
   var gbp;
 
   before("deploy escrow and token contracts", async function () {
     gbp = await GBP.new();
-    escrow = await Escrow.new(gbp.address, 1000, operator, receiver);
+    escrow = await Escrow.new(gbp.address, 1000);
+    await escrow.setRecipient(recipient);
   });
 
   it("should deposit", async function () {
@@ -18,7 +19,7 @@ contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
   });
 
   it("should not withdraw until unlocked", async function () {
-    await escrow.withdraw(receiver, 100, {from: receiver}).shouldBeReverted("VM Exception while processing transaction: revert Cannot withdraw more funds than has been unlocked");
+    await escrow.withdraw(recipient, 100, {from: recipient}).shouldBeReverted("VM Exception while processing transaction: revert Cannot withdraw more funds than has been unlocked");
   });
 
   it("should not unlock from unauthorised until unlocked", async function () {
@@ -26,36 +27,36 @@ contract('Escrow', function ([owner, operator, receiver, unauthorised]) {
   });
 
   it("should unlock", async function () {
-    await escrow.unlock(200, {from: operator});
+    await escrow.unlock(200, {from: owner});
     (await escrow.unlocked()).should.be.bignumber.equal('200');
   });
 
   it("should not withdraw from unauthorised", async function () {
-    await escrow.withdraw(receiver, 100, {from: unauthorised}).shouldBeReverted();
+    await escrow.withdraw(recipient, 100, {from: unauthorised}).shouldBeReverted();
   });
 
   it("should withdraw", async function () {
-    await escrow.withdraw(receiver, 100, {from: receiver});
+    await escrow.withdraw(recipient, 100, {from: recipient});
 
     (await escrow.withdrawn()).should.be.bignumber.equal('100');
     (await escrow.unlocked()).should.be.bignumber.equal('200');
 
     (await gbp.balanceOf(escrow.address)).should.be.bignumber.equal('900');
-    (await gbp.balanceOf(receiver)).should.be.bignumber.equal('100');
+    (await gbp.balanceOf(recipient)).should.be.bignumber.equal('100');
   });
 
   it("should not refund from unauthorised", async function () {
-    await escrow.refund(receiver, 100, {from: unauthorised}).shouldBeReverted();
+    await escrow.refund(recipient, 100, {from: unauthorised}).shouldBeReverted();
   });
 
   it("should not refund part that is unlocked", async function () {
-    await escrow.refund(operator, 900, {from: operator}).shouldBeReverted("Cannot refund more than is available");
+    await escrow.refund(owner, 900, {from: owner}).shouldBeReverted("Cannot refund more than is available");
   });
 
   it("should refund the remaining funds", async function () {
-    await escrow.refund(operator, 800, {from: operator});
+    await escrow.refund(owner, 800, {from: owner});
 
-    (await gbp.balanceOf(operator)).should.be.bignumber.equal('800');
+    (await gbp.balanceOf(owner)).should.be.bignumber.equal('800');
     (await gbp.balanceOf(escrow.address)).should.be.bignumber.equal('100');
     (await escrow.withdrawn()).should.be.bignumber.equal('100');
     (await escrow.unlocked()).should.be.bignumber.equal('200');
